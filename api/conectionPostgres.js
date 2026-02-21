93a5ac75c82b489508007a9e70932c5c349f972e
Geroncity2210 <juanpablomorenopatarroyo1@gmail.com>
2026-02-20 19:56:36 -0500

    Api implementation

---
+++ api/conectionPostgres.js
@@ -0,0 +1,9 @@
+import pg from "pg"
+
+export const pool = new pg.Pool({
+  host:"postgres-service",
+  port:5432,
+  database: "animalesdb",
+  user: "root",
+  password:"root"
+})
---
+++ api/index.js
@@ -0,0 +1,55 @@
+import express from "express"
+import { pool } from "./conectionPostgres.js";
+
+const app = express();
+app.use(express.json())
+const PORT = 8080;
+
+const getAnimales = async () => {
+  try {
+    const result = await pool.query("SELECT * FROM animales;");
+    console.log(`[CONSULTA] Todos los animales | Fecha/hora: ${new Date()}`)
+    return result;
+
+  } catch (error) {
+    console.error(error);
+    return false;
+
+  }
+}
+
+
+app.get("/animales", async (req, res) => {
+  const animales = await getAnimales();
+  animales === false ? res.status(500).json({ message: "Error desconocido en la db" }) : res.status(200).json({ animales: animales.rows });
+});
+
+app.post("/animales", async (req, res) => {
+  const {nombre} = req.body;
+  
+  if (!nombre) {
+  return res.status(400).json({ message: "El nombre es obligatorio" });
+  }
+
+  let suma = 0 
+  for (let i = 0; i < 1000; i++) {
+    for (let j = 0; j < 1000; j++) {
+      suma += j
+    }
+    suma += i;
+  }
+
+  try {
+    await pool.query("INSERT INTO animales (nombre) values ($1)", [nombre]);
+    console.log(`[INSERTADO] Animal ${nombre} | Fecha/hora: ${new Date()}`)
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Error desconocido en la db" })
+  }
+  res.status(201).json({ message: "Almacenado correctamente", nombre: nombre })
+})
+
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
